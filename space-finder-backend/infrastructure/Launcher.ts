5039864c52ab291fa3f99bfcd1a4976b35432a5f
Mark Greiner <mark.greiner@cec.mpg.de>
2022-12-25 20:02:37 +0100

    rebuilt project

--- space-finder-backend/infrastructure/Launcher.ts
+++
@@ -1,9 +0,0 @@
-// This file contains the logic for laucnhing the project
-
-import { SpaceStack } from './SpaceStack';
-import { App } from 'aws-cdk-lib';
-
-const app = new App()
-new SpaceStack(app, 'Space-finder', {
-    stackName: 'SpaceFinder'
-})
